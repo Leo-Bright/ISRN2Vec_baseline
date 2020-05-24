affe9e9b4e4719b24e9e0809bd760069b9a7816c
Leo Bright <buxingzhexiong@sina.com>
2020-05-24 09:24:05 +0800

    修改vector长度判读为小于2

--- joint_two_embeddings.py
+++ joint_two_embeddings.py
@@ -8,7 +8,7 @@ def get_emb_dic(emb_file_path):
             line = line.strip()
             _id_vector = line.split(' ')
             _id, _vector = _id_vector[0], _id_vector[1:]
-            if len(_vector) < 8:
+            if len(_vector) < 2:
                 continue
             emb_dic[_id] = _vector
 
@@ -24,7 +24,7 @@ def get_emb_ids(emb_file_path):
             line = line.strip()
             _id_vector = line.split(' ')
             _id, _vector = _id_vector[0], _id_vector[1:]
-            if len(_vector) < 8:
+            if len(_vector) < 2:
                 continue
             _ids.add(_id)
 
@@ -40,20 +40,21 @@ def joint_embs(src_emb_file_path, targ_emb_file_path, output_emb_path, method):
     error_counts = 0
     with open(output_emb_path, 'w+') as f:
         for _id in ids:
-        # try:
-            src_embedding = src_embeddings[_id]
-            tar_embedding = tar_embeddings[_id]
-            result_embedding = src_embedding +tar_embedding
-            f.write(_id + ' %s\n' % ' '.join(map(str, result_embedding)))
-        # except:
-        #     print("this osm_id encounter error:", _id)
-        #     error_counts += 1
-        # print("How many osm_id can not find embedding:", error_counts)
+            try:
+                src_embedding = src_embeddings[_id]
+                tar_embedding = tar_embeddings[_id]
+                result_embedding = src_embedding + tar_embedding
+                f.write(_id + ' %s\n' % ' '.join(map(str, result_embedding)))
+            except:
+                print("this osm_id encounter error:", _id)
+                error_counts += 1
+
+        print("How many osm_id can not find embedding:", error_counts)
 
 
 if __name__ == '__main__':
 
-    joint_embs(src_emb_file_path='sanfrancisco/node/sanfranciso_deepwalk_128d.embeddings',
-               targ_emb_file_path='sanfrancisco/node/sanfrancisco_combined_pca_traffic.embeddings',
-               output_emb_path='sanfrancisco/node/sanfrancisco_pca_and_deepwalk_128d_traffic.embeddings',
+    joint_embs(src_emb_file_path='sanfrancisco/node/sf_node2vec_128',
+               targ_emb_file_path='sanfrancisco/node/sanfrancisco_combined_pca_traffic_4d.embeddings',
+               output_emb_path='sanfrancisco/node/sanfrancisco_pca_4d_and_node2vec_128d_traffic.embeddings',
                method='&')
